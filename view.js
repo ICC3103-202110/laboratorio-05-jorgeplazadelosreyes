ab7413083b4ec5c220bac73afb4dd49406923a42
Jorge Plaza de los Reyes <japlazadelosreyes@miuandes.cl>
2021-05-16 18:08:53 -0400

    second commit

--- view.js
+++ view.js
@@ -1,3 +1,15 @@
 const inquirer = require('inquirer')
 const figlet = require('figlet')
 const chalk = require('chalk')
+
+function getTitle(){
+    return chalk.green(
+        figlet.textSync(
+            'Tip Calculator',
+            {
+                horizontalLayout: 'full',
+                font: 'Nancyj-Underlined'
+            }
+        )
+    )
+}
