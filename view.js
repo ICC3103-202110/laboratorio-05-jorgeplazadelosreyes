2da8f89b15320d8ac9913de9956c14910d9af4ae
Jorge Plaza de los Reyes <japlazadelosreyes@miuandes.cl>
2021-05-17 00:04:42 -0400

    second commit

--- view.js
+++ view.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer')
 const figlet = require('figlet')
 const chalk = require('chalk')
+const {printTable} = require('console-table-printer')
+
 
 function getTitle(){
     return chalk.green(
@@ -13,3 +15,31 @@ function getTitle(){
         )
     )
 }
+
+function getTable(i1,i2){
+    const bill = i1
+    const percent = i2
+    const tip = (bill/100) * percent
+    const total = bill + tip 
+    return [
+        {"Bill amount": bill},
+        {"Tip (%)": percent},
+        {"Tip": tip},
+        {"Total": total}
+    ]
+}
+
+function view(i1,i2){
+    return {
+        title: getTitle(),
+        table: getTable(i1,i2)
+    }
+}
+
+module.exports = {
+    view
+}
+const {title,table} = view(100,15)
+console.clear()
+console.log(title) 
+printTable(table)
